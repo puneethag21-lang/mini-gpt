88a912799d405872201367b767d68e6a993b9388
Puneetha <237708464+puneethag21-lang@users.noreply.github.com>
2026-08-27 17:06:14 +0530

    Update bigram.py

--- src/mini-gpt/bigram.py
+++ src/mini-gpt/bigram.py
@@ -1,1 +1,12 @@
-"""Bigram language model."""
+'''tokanizer classes'''
+class chartokenizer:
+    def __init__(self, text):
+        self.vocab = sorted(list(set(text)))
+        self.stoi = {ch: i for i, ch in enumerate(self.vocab)}
+        self.itos = {i: ch for i, ch in enumerate(self.vocab)}
+
+    def encode(self, text):
+        return [self.stoi[ch] for ch in text]
+
+    def decode(self, tokens):
+        return ''.join([self.itos[i] for i in tokens])
